72fc0bc42f7caee1e5f90a7021b0d396ef960743
Kleider David <klgonzalez@grupoasesores.com.mx>
2026-02-11 23:50:01 -0400

    Updates

---
+++ assets/index-DF6ROhVL.js
@@ -0,0 +1,6 @@
+import{c as n}from"./index-DJ9E0Ydt.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=n("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);function r(t,[e,a]){return Math.min(a,Math.max(e,t))}export{o as C,r as c};
---
+++ assets/textarea-CgO4JBZU.js
@@ -0,0 +1,11 @@
+import{c as e,r as t,j as o,a as d}from"./index-DJ9E0Ydt.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("Plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=e("Trash2",[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}],["line",{x1:"10",x2:"10",y1:"11",y2:"17",key:"1uufr5"}],["line",{x1:"14",x2:"14",y1:"11",y2:"17",key:"xtxkd"}]]),i=t.forwardRef(({className:a,...s},r)=>o.jsx("textarea",{className:d("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",a),ref:r,...s}));i.displayName="Textarea";export{c as P,l as T,i as a};
