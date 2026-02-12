1cd35100485b7743e8c3dafb9c275556b5acaca6
Kleider David <klgonzalez@grupoasesores.com.mx>
2026-02-11 23:42:28 -0400

    ajustado githubpages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/virtual-clinic-manager/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
